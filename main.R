b55305a630b859d5c284cb838de88e400923816c
agouy <contact@agouy.com>
2020-10-10 17:00:16 +0000

    first version

--- main.R
+++ main.R
@@ -1,9 +1,36 @@
 library(tercen)
 library(dplyr)
+library(tidyr)
+library(GSA)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+doc_to_data = function(df){
+  filename = tempfile()
+  writeBin(ctx$client$fileService$download(df$documentId[1]), filename)
+  on.exit(unlink(filename))
+  
+  dat <- GSA::GSA.read.gmt(filename)
+  dat$genesets <- lapply(dat$genesets, paste, collapse = "")
+  
+  df_out <- data.frame(
+    set_name = dat$geneset.names,
+    set_description = dat$geneset.descriptions,
+    set_genes = unlist(dat$genesets)
+  ) %>% 
+    mutate(set_genes = strsplit(as.character(set_genes), ",")) %>% 
+    unnest(set_genes) %>%
+    mutate(.ci= rep_len(df$.ci[1], nrow(.)))
+  
+  return(df_out)
+}
+
+ctx = tercenCtx()
+
+if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
+
+df <- ctx$cselect() %>% 
+  mutate(.ci= 1:nrow(.)-1) %>%
+  split(.$.ci) %>%
+  lapply(doc_to_data) %>%
+  bind_rows() %>%
   ctx$addNamespace() %>%
   ctx$save()
